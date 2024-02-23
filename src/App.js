21335ba66ae17fb4e15e993d4e1e65333359d28a
inapogui <153550531+Michel-2a@users.noreply.github.com>
2024-02-23 22:02:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider, Outlet
+} from "react-router-dom";
+import Navigation from './pages/customer/components/Navigation';
+import Footer from './pages/customer/components/Footer';
+import Index from './pages/customer/Index';
+import Register from './pages/customer/auth/Register';
+import Login from './pages/customer/auth/Login';
+import Error from './pages/customer/Error';
+import Signin from './pages/admin/auth/Signin';
+import Signup from './pages/admin/auth/Signup';
+import Home from './pages/admin/Home';
+import Pages from './pages/customer/components/Pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const   Layout = ()  => {
+  return(
+    <> 
+    <Navigation /> 
+    <Outlet /> 
+    <Footer/> 
+    </>
+  )
 }
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element:  <Layout/>,
+    children: [
+      {
+        path: "/",
+        element:  < Index/> 
+      },
+      {
+        path: "/single",
+        element:  < Pages/> 
+      },
+    ],
+  },
+  {
+    path: "/auth/user/register",
+        element:  < Register/> 
+  },
+  {
+    path: "/auth/user/login",
+        element:  < Login/> 
+  }, 
+  {
+    path: "*",
+        element:  < Error/> 
+  },
+// Les leins vers les dossier Admin avec les connexion de admin
+  {
+    path: "/admin/property",
+        element:  < Home/> 
+  }, 
+  //Connexion et inscription de admin
+  {
+    path: "/auth/owner/signin",
+        element:  < Signin/> 
+  }, 
+  {
+    path: "/auth/owner/signup",
+        element:  < Signup/> 
+  }, 
+])
+
+const App = () => {
+  return (
+    <>
+     < RouterProvider  router = {router}/>
+    </>
+  );
+};
 
 export default App;
